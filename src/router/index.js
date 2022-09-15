ed613111e25e61749fa227a3ff69ff79df2ad852
Biel Serrano Sanchez <bielsesa@gmail.com>
2022-09-15 07:32:04 +0200

    changed favicon and added some code

--- src/router/index.js
+++ src/router/index.js
@@ -17,6 +17,14 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import("../views/AboutView.vue"),
     },
+    {
+      path: "/summary",
+      name: "summary",
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import("../views/SummaryProjectView.vue"),
+    },
   ],
 });
 
